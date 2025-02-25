1cfb8e0e421624c4d3e00fe8894340cc0df07ec2
Torathion <torathion@gmail.com>
2025-02-25 20:54:41 +0100

    dev: update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import shiny from 'eslint-config-shiny'
 
-export default await shiny({ configs: ['node', 'format', 'vitest'], indent: true })
+export default [
+    ...await shiny({ configs: ['node', 'format', 'vitest'], indent: true }),
+    {
+        rules: {
+            'unicorn/prefer-at': 0,
+            'unicorn/no-nested-ternary': 0
+        }
+    }
+]
